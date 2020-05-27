3d4a0da83b41f8a0991402c539e75a6b9d27ee58
Michal Nowak <michalkacpernowak@gmail.com>
2020-05-27 18:54:50 +0200

    Tutorial finished

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace BankAccount
 		{
 			var account = new BankAccount("Michał", 1000);
 			Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+			account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+			Console.WriteLine(account.Balance);
+			account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+			Console.WriteLine(account.Balance);
+
+			Console.WriteLine(account.GetAccountHistory());
 		}
 	}
 }
