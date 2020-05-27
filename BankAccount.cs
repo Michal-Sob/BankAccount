45ad4e93ab937fa54ac382be8a961a069087f454
Michal Nowak <michalkacpernowak@gmail.com>
2020-05-27 16:51:25 +0200

    Basic Start (already with Transactions)

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+	class BankAccount
+	{
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankAccount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var account = new BankAccount("Michał", 1000);
+			Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+		}
+	}
+}
